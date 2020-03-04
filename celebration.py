31aa3879dd64118a9281dc41d652e1677468f744
Quinlan Pfiffer <qpfiffer@gmail.com>
2020-03-04 10:58:11 -0800

    python 3 and screaming

--- celebration.py
+++ celebration.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
+from functools import reduce
+import re
 
 # Chain of days
 # Most people per day
@@ -17,11 +19,15 @@ class SetEncoder(json.JSONEncoder):
 
 def main():
     opened = open("./irc.esper.#merveilles.weechatlog")
+    compiled = re.compile(r"\tA*$")
 
     dates_celebrated_by_who = defaultdict(set)
+    screaming_moments_by_who = defaultdict(set)
     who_chain = defaultdict(lambda: 0)
+    screaming_chain = defaultdict(lambda: 0)
 
     found_first = False
+    screaming_found_first = False
     most_recent_date = None
     for line in opened:
         date_str = line[:19]
@@ -35,6 +41,15 @@ def main():
 
             if found_first is False:
                 found_first = {"when": key, "who": username}
+        elif re.search(r"\tA+$", line) is not None:
+            split = line.split("\t")
+            username = split[1].replace("@", "")
+            key = '{}-{}-{}'.format(date_obj.year, date_obj.month, date_obj.day)
+            screaming_moments_by_who[key].add(username)
+            screaming_chain[username] += 1
+
+            if screaming_found_first is False:
+                screaming_found_first = {"when": key, "who": username}
         most_recent_date = date_obj
     opened.close()
 
@@ -45,12 +60,16 @@ def main():
             "days_since_first_celebration": delta.days,
             "celebration_score": "{}/{}".format(times_celebrated, delta.days),
             "celebrated_count": who_chain,
+            "screaming_count": screaming_chain,
             "first_celebrated": found_first,
-            "who_had_most_celebrations": reduce(lambda accum, val: accum if accum[1] > val[1] else val, who_chain.iteritems(), ("x", -1)),
-            "when_had_most_celebrations": reduce(lambda accum, val: accum if len(accum[1]) > len(val[1]) else val, dates_celebrated_by_who.iteritems(), ("x", [])),
+            "first_scream_celebrated": screaming_found_first,
+            "who_had_most_celebrations": reduce(lambda accum, val: accum if accum[1] > val[1] else val, who_chain.items(), ("x", -1)),
+            "when_had_most_celebrations": reduce(lambda accum, val: accum if len(accum[1]) > len(val[1]) else val, dates_celebrated_by_who.items(), ("x", [])),
+            "who_had_most_screams": reduce(lambda accum, val: accum if accum[1] > val[1] else val, screaming_chain.items(), ("x", -1)),
+            "when_had_most_screams": reduce(lambda accum, val: accum if len(accum[1]) > len(val[1]) else val, screaming_moments_by_who.items(), ("x", [])),
             "longest_streak": "???"
             }
-    print json.dumps(final, cls=SetEncoder)
+    print(json.dumps(final, cls=SetEncoder))
 
 
 if __name__ == '__main__':
